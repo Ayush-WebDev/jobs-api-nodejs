5d3ee7261cb85295f5dcb92ff4359cd303728428
ayush kumar <ayuk451998@gmail.com>
2024-05-26 20:48:59 +0530

    changed routes

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ userRouter.get("/", (req, res) => {
   );
 });
 
-userRouter.post("/register", registerUser);
-userRouter.post("/login", loginUser);
+userRouter.post("/api/v1/register", registerUser);
+userRouter.post("/api/v1/login", loginUser);
 
 module.exports = userRouter;
